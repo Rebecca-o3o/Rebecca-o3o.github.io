fbd613b866b0abb1158fde3040bb7f3df7fd70da
Rebecca-o3o <rebecca.krummel@gmail.com>
2020-08-20 11:41:37 +0200

    2020 update with 2 new projects

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Pesonal Website
+
+## Overview
+Using JavaScript, Sass with gulp.
+
+## Installation
+After packages are installed with ```npm install```, run ```gulp sass```. Start a server to view in browser  (e.g. ```http-server```).
+
+## Deployment
+Do commit compiled css files as hosted on github pages. 
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var concat = require('gulp-concat')
-
-gulp.task('sass', function() {
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('master.css'))
-    .pipe(gulp.dest('./css/'))
-})
-
-//Watch task
-// gulp.task('default',function() {
-//   return gulp.watch('./sass/**/*.scss',['sass'])
-// })
-
-
-gulp.task('default',['sass'])
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
